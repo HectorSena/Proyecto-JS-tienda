cfb28f7b2acfe8ceee3c0b8d13a8bfb1a09178f9
Hector Sena <hsena11@outlook.com>
2023-01-17 23:18:48 -0300

    primer commit

---
+++ pluggins/sweetAlert.js
@@ -0,0 +1,10 @@
+/* Area sweet alert 2 */
+
+Swal.fire({
+    title: "Bienvenido !",
+    timer: 3000,
+    timerProgressBar: true,
+    position: "bottom",
+    allowOutsideClick: true,
+    confirmButtonText: "Continuar",
+})
