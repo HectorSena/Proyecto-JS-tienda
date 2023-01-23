f67228d26caa6bf7b20e05a0e162dfbd8358672f
Hector Sena <hsena11@outlook.com>
2023-01-22 23:17:00 -0300

    se realizo la version mobile

--- js/carrito.js
+++ js/carrito.js
@@ -1,14 +1,14 @@
-let productosEnCarrito = localStorage.getItem("productos-en-carrito");
-productosEnCarrito = JSON.parse(productosEnCarrito);
+let productosEnCarrito = localStorage.getItem("productos-en-carrito")
+productosEnCarrito = JSON.parse(productosEnCarrito)
 
-const contenedorCarritoVacio = document.querySelector("#carrito-vacio");
-const contenedorCarritoProductos = document.querySelector("#carrito-productos");
-const contenedorCarritoAcciones = document.querySelector("#carrito-acciones");
-const contenedorCarritoComprado = document.querySelector("#carrito-comprado");
-let botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar");
-const botonVaciar = document.querySelector("#carrito-acciones-vaciar");
-const contenedorTotal = document.querySelector("#total");
-const botonComprar = document.querySelector("#carrito-acciones-comprar");
+const contenedorCarritoVacio = document.querySelector("#carrito-vacio")
+const contenedorCarritoProductos = document.querySelector("#carrito-productos")
+const contenedorCarritoAcciones = document.querySelector("#carrito-acciones")
+const contenedorCarritoComprado = document.querySelector("#carrito-comprado")
+let botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar")
+const botonVaciar = document.querySelector("#carrito-acciones-vaciar")
+const contenedorTotal = document.querySelector("#total")
+const botonComprar = document.querySelector("#carrito-acciones-comprar")
 
 
 function cargarProductosCarrito() {
